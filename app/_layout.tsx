6732324bed51ca2a9c0744994e8a6f2246a1c28d
AmineBelho21 <amine.belhocine@fastdelivery.dz>
2025-09-24 17:29:49 +0200

    clerk layout setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,44 @@
-import { Stack } from "expo-router";
+import { tokenCache } from "@/utlis/cache";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { DMSans_400Regular, DMSans_500Medium, DMSans_700Bold, useFonts } from "@expo-google-fonts/dm-sans";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+if (!clerkPublishableKey) {
+  throw new Error("Missing Publishable Key. Please set the EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY environment variable.");
+}
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular, 
+    DMSans_500Medium,
+    DMSans_700Bold
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  return (
+    <Slot />
+  );
+}
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={clerkPublishableKey!} 
+      tokenCache={tokenCache} 
+    >
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
