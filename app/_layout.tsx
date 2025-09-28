2c57dbe96e59e0f4c2c78d83512cd977d4ab1f36
AmineBelho21 <amine.belhocine@fastdelivery.dz>
2025-09-28 18:07:16 +0200

    feat: Integrate Sentry for error tracking and monitoring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,26 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { Slot, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://7cfc51944203a25cd6a9200fec787920@o4510098045599744.ingest.de.sentry.io/4510098056478800',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const clerkPublishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 if (!clerkPublishableKey) {
@@ -54,7 +74,7 @@ const InitialLayout = () => {
 }
 
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   return (
     <ClerkProvider publishableKey={clerkPublishableKey!} 
       tokenCache={tokenCache} 
@@ -66,4 +86,4 @@ export default function RootLayout() {
       </ClerkLoaded>
     </ClerkProvider>
   );
-}
+});
